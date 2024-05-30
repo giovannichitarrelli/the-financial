081614827278468bf9d030ad6dbbbca19aff9974
Giovanni <giovanni.chitarrelli@gmail.com>
2024-05-30 20:29:25 -0300

    feat: add layout settings

---
+++ app/app/settings/layout.tsx
@@ -0,0 +1,24 @@
+import { PropsWithChildren } from "react";
+import { SettingsSidebar } from "./_components/settings-sidebar";
+import {
+  DashboardPage,
+  DashboardPageHeader,
+  DashboardPageHeaderTitle,
+  DashboardPageMain,
+} from "../_components/dashboard/page";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <DashboardPage>
+      <DashboardPageHeader>
+        <DashboardPageHeaderTitle>Configurações</DashboardPageHeaderTitle>
+      </DashboardPageHeader>
+      <DashboardPageMain>
+        <div className="container max-w-screen-lg">
+          <SettingsSidebar />
+          <div>{children}</div>
+        </div>
+      </DashboardPageMain>
+    </DashboardPage>
+  );
+}
