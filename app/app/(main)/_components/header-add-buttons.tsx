69a46c804768806551a64539d2277557c190a672
Giovanni <giovanni.chitarrelli@gmail.com>
2024-05-30 13:58:28 -0300

    feat: add header add buttons component

---
+++ app/app/(main)/_components/header-add-buttons.tsx
@@ -0,0 +1,12 @@
+import { ExpensesUpsertSheet } from "../../_components/_upserts/expenses-upersert-sheet";
+import { SalaryUpsertSheet } from "../../_components/_upserts/salary-upersert-sheet";
+import { DashboardPageHeaderNav } from "../../_components/dashboard/page";
+
+export function HeaderAddButtons() {
+  return (
+    <DashboardPageHeaderNav className="hidden items-center space-x-2 lg:flex">
+      <ExpensesUpsertSheet />
+      <SalaryUpsertSheet />
+    </DashboardPageHeaderNav>
+  );
+}
