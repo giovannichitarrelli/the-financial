df074c3cd45ceebb546e36a4ef6f44f0a7328b00
Giovanni <giovanni.chitarrelli@gmail.com>
2024-05-30 13:38:45 -0300

    feat: add layout dashboard

---
+++ app/app/layout.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from "react";
+import { MainSideBar } from "./_components/main-sidebar";
+import { auth } from "@/app/_lib/auth";
+
+export default async function Layout({ children }: PropsWithChildren) {
+  const session = await auth();
+
+  return (
+    <div className="grid w-full lg:grid-cols-[270px_1fr] ">
+      <div className="lg:relative">
+        <div className=" lg:fixed lg:max-w-[270px] ">
+          <MainSideBar user={session?.user} />
+        </div>
+      </div>
+
+      <main>{children}</main>
+    </div>
+  );
+}
