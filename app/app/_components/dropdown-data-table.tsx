5c9c21b23790746ba25cf2009835a4e16ed0ebcf
Giovanni <giovanni.chitarrelli@gmail.com>
2024-05-30 12:55:54 -0300

    feat: add dropdown  data table component

---
+++ app/app/_components/dropdown-data-table.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+import { Button } from "@/app/_components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/app/_components/ui/dropdown-menu";
+import { MoreHorizontal, CircleCheckBig, ArchiveX } from "lucide-react";
+import * as React from "react";
+
+type DropdownMenuProps = {
+  onToggleDone?: (item: any) => void;
+  onDelete?: (item: any) => void;
+};
+
+const DropdownMenuComponent: React.FC<DropdownMenuProps> = ({
+  onToggleDone,
+  onDelete,
+}) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="secondary" className="h-8 w-8 p-0">
+          <span className="sr-only">Abrir menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {onToggleDone && (
+          <DropdownMenuItem
+            className="flex items-center"
+            onClick={onToggleDone}
+          >
+            <CircleCheckBig className="mr-3 h-4 w-4" /> Alterar status
+          </DropdownMenuItem>
+        )}
+        {onDelete && (
+          <DropdownMenuItem className="flex items-center" onClick={onDelete}>
+            <ArchiveX className="mr-3 h-4 w-4" /> Deletar
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default DropdownMenuComponent;
