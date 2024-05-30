75c5a6a50bc6bb9dd0dae44649aff55e13df2fbc
Giovanni <giovanni.chitarrelli@gmail.com>
2024-05-30 13:54:53 -0300

    feat: add charts cards component

---
+++ app/app/(main)/_components/charts-cards.tsx
@@ -0,0 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/app/_components/ui/card";
+import { ReactElement } from "react";
+
+interface CardsProps {
+  title: string;
+  month?: ReactElement<any>;
+  component: ReactElement<any, any>;
+}
+
+const ChartsCards = ({ title, component, month }: CardsProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2">
+          {title} {month}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>{component}</CardContent>
+    </Card>
+  );
+};
+
+export default ChartsCards;
