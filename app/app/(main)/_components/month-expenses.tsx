ca1c2e08f4b145993ba3d6681cd9446a15556089
Giovanni <giovanni.chitarrelli@gmail.com>
2024-05-30 13:45:15 -0300

    feat: add card month expenses component

---
+++ app/app/(main)/_components/month-expenses.tsx
@@ -0,0 +1,32 @@
+import { formatCurrency } from "../../_components/_helpers/formatCurrency";
+import { getUserMonthExpenses } from "../../despesas/actions";
+import { StatusPayment } from "../../_components/status";
+import MsgNoData from "../../_components/no-data-table";
+
+export async function MonthExpenses() {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentYear = currentDate.getFullYear();
+  const expenses = await getUserMonthExpenses(currentMonth, currentYear);
+
+  return (
+    <div>
+      {expenses.length > 0 ? (
+        expenses.slice(-5).map((expenses) => (
+          <div key={expenses.id} className="grid grid-cols-4 py-3">
+            <p className="text-md col-span-2 font-bold">{expenses.title}</p>
+            <div>{formatCurrency(Number(expenses.price))}</div>
+            <div className="text-right">
+              <StatusPayment
+                doneAt={expenses.doneAt}
+                expiryAt={expenses.expiryAt}
+              />
+            </div>
+          </div>
+        ))
+      ) : (
+        <MsgNoData />
+      )}
+    </div>
+  );
+}
