8e851e3242c00d3f6e7bc8ac3bad06d85fffb884
Giovanni <giovanni.chitarrelli@gmail.com>
2024-05-30 21:00:43 -0300

    feat: add filter component investments

---
+++ app/app/investimentos/_components/filter-component-investments.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { useState } from "react";
+import { getUserMonthInvestments, getUserMonthWithdraws } from "../actions";
+import {
+  getTotalMonthSaved,
+  getTotalSaved,
+} from "../../_components/_helpers/totals";
+import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
+import { ArrowRightCircle, HandCoins } from "lucide-react";
+import { InvestmentsDataTable } from "./investments-data-table";
+import { WithdrawsDataTable } from "./withdraw-data-table";
+import { Investments, Withdraw } from "../../types";
+import {
+  SelectMonthFilter,
+  SelectYearPickerFilter,
+} from "../../_components/select-picker";
+import MsgNoData from "../../_components/no-data-table";
+
+interface ClientComponentProps {
+  initialMonth: string;
+  initialYear: string;
+  initialInvestments: Investments[];
+  initialWithdraws: Withdraw[];
+  initialTotalSaved: string;
+  initialTotalMonthSaved: string;
+}
+
+export default function FilterComponentInvestments({
+  initialMonth,
+  initialYear,
+  initialInvestments,
+  initialWithdraws,
+  initialTotalSaved,
+  initialTotalMonthSaved,
+}: ClientComponentProps) {
+  const [selectedMonth, setSelectedMonth] = useState<string>(initialMonth);
+  const [selectedYear, setSelectedYear] = useState<string>(initialYear);
+  const [investments, setInvestments] = useState(initialInvestments);
+  const [withdraws, setWithdraws] = useState(initialWithdraws);
+  const [totalSaved, setTotalSaved] = useState(initialTotalSaved);
+  const [totalMonthSaved, setTotalMonthSaved] = useState(
+    initialTotalMonthSaved,
+  );
+
+  const fetchDataForMonthYear = (month: number, year: number) => {
+    Promise.all([
+      getUserMonthInvestments(month, year),
+      getUserMonthWithdraws(month, year),
+      getTotalSaved(),
+      getTotalMonthSaved(month, year),
+    ])
+      .then(
+        ([
+          investmentsData,
+          withdrawsData,
+          totalSavedData,
+          totalMonthSavedData,
+        ]) => {
+          setInvestments(investmentsData);
+          setWithdraws(withdrawsData);
+          setTotalSaved(totalSavedData);
+          setTotalMonthSaved(totalMonthSavedData);
+        },
+      )
+      .catch((error) => {
+        console.error("Erro ao buscar dados de investimentos:", error);
+      });
+  };
+
+  const handleYearSelect = (selectedYear: string) => {
+    setSelectedYear(selectedYear);
+    fetchDataForMonthYear(parseInt(selectedMonth), parseInt(selectedYear));
+  };
+
+  const handleMonthSelect = (selectedMonth: string) => {
+    setSelectedMonth(selectedMonth);
+    fetchDataForMonthYear(parseInt(selectedMonth), parseInt(selectedYear));
+  };
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2">
+          <SelectMonthFilter onSelectMonth={handleMonthSelect} />
+          <SelectYearPickerFilter onSelectYear={handleYearSelect} />
+        </div>
+      </div>
+
+      <Card>
+        <CardContent className="flex items-center justify-between py-2">
+          <p className="flex items-center">
+            <HandCoins className="mr-2 h-4 w-4" /> Saldo total:
+            <span className="ml-2 font-bold text-blue-500">{totalSaved}</span>
+          </p>
+          <p className="flex items-center">
+            <HandCoins className="mr-2 h-4 w-4" /> Saldo mensal:
+            <span className="ml-2 font-bold text-blue-500">
+              {totalMonthSaved}
+            </span>
+          </p>
+        </CardContent>
+      </Card>
+
+      {investments.length || withdraws.length > 0 ? (
+        <>
+          <InvestmentsDataTable data={investments} />
+          <Card className="border-red-500 shadow-red-500">
+            <CardHeader>
+              <div className="flex items-center">
+                <h3 className="text-lg font-bold"> Saques / Prejuízos</h3>
+                <ArrowRightCircle className="ml-2 h-4 w-4 text-red-500" />
+              </div>
+            </CardHeader>
+
+            {withdraws.length > 0 ? (
+              <WithdrawsDataTable data={withdraws} />
+            ) : (
+              <MsgNoData />
+            )}
+          </Card>
+        </>
+      ) : (
+        <MsgNoData />
+      )}
+    </>
+  );
+}
