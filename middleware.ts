4f973c8be4c4d366776c47b231bd6e4b0ca76e8f
Giovanni <giovanni.chitarrelli@gmail.com>
2024-06-01 13:30:04 -0300

    fix: updated middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { getUrl } from "./app/_lib/get-url";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("authjs.session-token");
+  // const token = request.cookies.get("authjs.session-token");
+  const token =
+    request.cookies.get("next-auth.session-token")?.value ||
+    request.cookies.get("__Secure-next-auth.session-token")?.value;
   const pathname = request.nextUrl.pathname;
 
   if (pathname === "/auth" && token) {
@@ -19,3 +22,12 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+// import { NextRequest, NextResponse } from "next/server";
+
+// export default function middleware(req: NextRequest) {
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// };
