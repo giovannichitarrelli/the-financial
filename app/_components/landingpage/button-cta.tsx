2d9aa8b2cff3be6d62feee5811eb0e1ae2356447
Giovanni <giovanni.chitarrelli@gmail.com>
2024-06-07 15:05:16 -0300

    fix: update arrow

--- app/_components/landingpage/button-cta.tsx
+++ app/_components/landingpage/button-cta.tsx
@@ -10,7 +10,8 @@ const ButtonCta = ({ cta }: ButtonProps) => {
     <Link href="/auth">
       <Button className="text-md rounded-sm shadow-md shadow-green-800 brightness-100  ">
         {cta}
-        <SendHorizonal className="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-2 group-hover:opacity-100 " />
+        {/* <SendHorizonal className="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-2 group-hover:opacity-100 " /> */}
+        <SendHorizonal className="mr-2 h-3 w-3 " />
       </Button>
     </Link>
   );
