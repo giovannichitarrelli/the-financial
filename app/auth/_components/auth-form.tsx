04e12ff4db333fb1bb155a3d29650c66f55a60a5
Giovanni <giovanni.chitarrelli@gmail.com>
2024-06-07 13:03:26 -0300

    style: login page

--- app/auth/_components/auth-form.tsx
+++ app/auth/_components/auth-form.tsx
@@ -1,19 +1,16 @@
 "use client";
 import { Icons } from "@/app/_components/icons";
-import Logo from "@/app/_components/logo";
 import { Button } from "@/app/_components/ui/button";
 import { signIn } from "next-auth/react";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/app/_components/ui/card";
 import { Loader2 } from "lucide-react";
-import Link from "next/link";
+// import Link from "next/link";
 import React from "react";
 import { toast } from "sonner";
-
+import Image from "next/image";
+import Logo from "@/app/_components/logo";
+import { Card, CardContent, CardHeader } from "@/app/_components/ui/card";
+// import { Input } from "@/app/_components/ui/input";
+// import { Label } from "@/app/_components/ui/label";
 const AuthForm = () => {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
@@ -30,37 +27,84 @@ const AuthForm = () => {
       description: "Por favor, aguarde...",
     });
   }
-
   return (
-    <Card className=" w-[400px] max-w-full p-10">
-      <Link href="/">
-        <Logo />
-      </Link>
-      <CardHeader>
-        <CardTitle>Acesse sua conta</CardTitle>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <Button
-          className="w-full"
-          variant="outline"
-          disabled={isLoading}
-          onClick={handleLoginGoogle}
-        >
-          {isLoading ? (
-            <>
-              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-              <Icons.google className="mr-2 h-4 w-4" />
-              Entrar com Google
-            </>
-          ) : (
-            <>
-              <Icons.google className="mr-2 h-4 w-4" />
-              Entrar com Google
-            </>
-          )}
-        </Button>
-      </CardContent>
-    </Card>
+    <div className="min-h-[100vh] w-full  lg:grid lg:grid-cols-2">
+      <div className="flex items-center justify-center py-12">
+        <Card className="mx-auto  grid w-[350px] gap-6">
+          <CardHeader className="grid gap-2 text-center">
+            <div className="mx-auto">
+              <Logo />
+            </div>
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              {/* Insira o email abaixo para fazer login na sua conta */}
+              Faça login com google
+            </p>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            {/* <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="email@dindin.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Senha</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Esqueceu a senha?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button> */}
+
+            <Button
+              className="w-full"
+              variant="outline"
+              disabled={isLoading}
+              onClick={handleLoginGoogle}
+            >
+              {isLoading ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  <Icons.google className="mr-2 h-4 w-4" />
+                  Login com Google
+                </>
+              ) : (
+                <>
+                  <Icons.google className="mr-2 h-4 w-4" />
+                  Login com Google
+                </>
+              )}
+            </Button>
+          </CardContent>
+          {/* <div className="mt-4 text-center text-sm">
+            Não tem uma conta?{" "}
+            <Link href="#" className="underline">
+              Registre-se
+            </Link>
+          </div> */}
+        </Card>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/login-dindin.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover "
+        />
+      </div>
+    </div>
   );
 };
 
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,3 @@
-import Footer from "@/app/_components/footer";
 import AuthForm from "./_components/auth-form";
 import { Metadata } from "next";
 export const metadata: Metadata = {
@@ -6,14 +5,5 @@ export const metadata: Metadata = {
   description: "Página de login do app DinDin.",
 };
 export default function Page() {
-  return (
-    <>
-      <div className="flex min-h-[80vh] w-full items-center justify-center px-4 py-12">
-        <div className="mx-auto w-[400px] max-w-full space-y-4">
-          <AuthForm />
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+  return <AuthForm />;
 }
