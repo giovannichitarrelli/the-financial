4f61900f57684da47001ecc9f10d3c381ab48e39
Giovanni <giovanni.chitarrelli@gmail.com>
2025-01-30 00:15:34 -0300

    fix: remove types

--- app/app/types.ts
+++ app/app/types.ts
@@ -1,9 +1,9 @@
-import { ReturnTypeWithoutPromise } from "@/types/return-type-without-promise";
+// import { ReturnTypeWithoutPromise } from "@/types/return-type-without-promise";
 
-import { getUserInvestments } from "./investimentos/actions";
-import { getUserWishlist } from "./metas/actions";
+// import { getUserInvestments } from "./investimentos/actions";
+// import { getUserWishlist } from "./metas/actions";
 
-export type Investments = ReturnTypeWithoutPromise<
-  typeof getUserInvestments
->[0];
-export type Wishlist = ReturnTypeWithoutPromise<typeof getUserWishlist>[0];
+// export type Investments = ReturnTypeWithoutPromise<
+//   typeof getUserInvestments
+// >[0];
+// export type Wishlist = ReturnTypeWithoutPromise<typeof getUserWishlist>[0];
